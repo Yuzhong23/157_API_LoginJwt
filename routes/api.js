ea98e933e26606597e84b65b4a877f41733665be
Yuzhong23 <fatursetia123@gmail.com>
2026-07-27 10:54:09 +0700

    feat: mengatur routing untuk endpoint komik dan user beserta perlindungan middleware

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const komikController = require('../controller/komikController');
 
-// Mendaftarkan URL (endpoint) untuk operasi CRUD Komik
-router.get('/komik', komikController.getAllKomik);
-router.get('/komik/:id', komikController.getKomikById);
-router.post('/komik', komikController.createKomik);
-router.put('/komik/:id', komikController.updateKomik);
-router.delete('/komik/:id', komikController.deleteKomik);
+const komikController = require("../controller/komikController");
+const userController = require("../controller/userController");
+const authMiddleware = require("../middleware/authMiddleware");
+
+router.post("/register", userController.register);
+router.post("/login", userController.login);
+
+// public
+router.get("/komik", komikController.getAllKomik);
+router.get("/komik/:id", komikController.getKomikById);
+
+// Protected
+router.post("/komik", authMiddleware, komikController.createKomik);
+router.put("/komik/:id", authMiddleware, komikController.updateKomik);
+router.delete("/komik/:id", authMiddleware, komikController.deleteKomik);
 
 module.exports = router;
